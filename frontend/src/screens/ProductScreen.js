31f6ced242ea8270675ae2ace81cd984a2fe22b6
Some Guy <nope@nope.com>
2020-11-29 18:28:10 -0500

    Express backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 
 const ProductScreen = ({match }) => {
     
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
     
+    useEffect(() => {
+            
+        const fetchProduct = async () => {
+
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+            setProduct(data)
+
+        }
+
+        fetchProduct()
+
+    }, [match])
     
     return (
         <>
@@ -28,7 +42,7 @@ const ProductScreen = ({match }) => {
                         Price: ${product.price}
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        Description: ${product.description}
+                        Description: {product.description}
                     </ListGroup.Item>
                 </ListGroup>
             </Col>
